056a32f48e7d2046aa05026986c233d7ea3abb55
César Castro <cesar.castro@g.xarevision.pt>
2024-12-06 15:30:37 +0000

    updated storybook to watch npm link modules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
     "@storybook/addon-onboarding",
@@ -10,12 +10,41 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: '@storybook/react-webpack5',
     options: {},
   },
-  babel: async (options) => {
-    options.presets.push('@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript');
-    return options;
+  webpackFinal: async (config) => {
+    config.resolve?.symlinks ? (config.resolve.symlinks = true) : null; // enable symlinks
+    config.module?.rules?.push({
+      test: /\.(ts|tsx)$/,
+      exclude: /node_modules\/(?!(shared-utils)\/).*/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+
+    config.resolve = {
+      ...config.resolve,
+      extensions: ['.ts', '.tsx', ...(config.resolve?.extensions || [])],
+    };
+
+    return config;
   },
 };
+
 export default config;
